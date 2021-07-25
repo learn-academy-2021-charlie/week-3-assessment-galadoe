41b3ab9ed226478323ed4c3631232e9bce703fcd
galadoe <kgaladoe@gmail.com>
2021-07-24 22:02:56 -0700

    continue working on assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -16,7 +16,14 @@
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array of that length containing the numbers of the Fibonacci.
 
+
 // a) Create a test with expect statements for each of the variables provided.
+describe("FibNum", () => {
+    test("returns an array the length containg the num of fib", () => {
+      expect(fibNum(6)).toEqual([1, 1, 2, 3, 5, 8])
+      expect(fibNum(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+})
 
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
@@ -27,13 +34,28 @@
 
 
 // b) Create the function that makes the test pass.
+function fibNum (num) {
+  let fib = [0, 1]
+  let fibNumber = []
 
+  for(let i = 2; i <= num; i++){
+    fib[i] = fib[i - 1] + fib[i - 2];
+    fibNumber.push(fib[i]);
+  }
+  return fibNumber;
+}
+console.log(fibNum(10));
 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
-
+describe("leastToGreatest", () => {
+  it("returns odd numbers l to g", () => {
+    expect(leastToGreatest(fullArr1)).toEqual([-9, 7, 9, 199])
+    expect(leastToGreatest(fullArr2)).toEqual([-823, 7, 23])
+  })
+})
 var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
 
@@ -43,11 +65,48 @@ var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 
 // b) Create the function that makes the test pass.
 
+//create a function and takes an array
+//create a for loop that loops through the array looking for typeOf number (try to use if you can use a higer order function)
+//once all the typeOf numbers are found, put it in order from least to leastToGreatest
+//return the new array of numbers put in order from least to greatest
+
+const leastToGreatest = (nums) => {
+  let oddNums = []
+  for(let i = 0; i < nums.length; i++){
+    if(typeof nums[i] === "number" && nums[i] % 2 !== 0){
+      oddNums.push(nums[i])
+    }
+  }
+  return oddNums.sort((a,b) => a - b)
+}
+console.log(leastToGreatest(fullArr1));
+console.log(leastToGreatest(fullArr2));
+
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("sum", () => {
+  it("returns an array of accumlating sum", () => {
+    expect(arraySum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+    expect(arraySum(numbersToAdd2)).toEqual([0, 7, -8, 12])
+    expect(arraySum(numbersToAdd3)).toEqual([])
+  })
+})
+
+//create a function that takes in an array
+//the array is iterated through
+//the previous index is compared to the next index
+//those two index are added and that number is saved and printed (as an array)
+//the saved num is add to the next num in the arrays and printed as well
+//continue to do this until the array is over
+
+const arraySum = (arrayNum) => {
+  let newSum = []
+  let sum = 0
+
+}
 
 var numbersToAdd1 = [2, 4, 45, 9]
 // Excpected output: [2, 6, 51, 60]
